c0baa610013359ce61b32c63d1f92aad4a4ffedf
Saurabh Joshi <saurabhjoshi7869@gmail.com>
2023-05-30 22:17:36 +0530

    initial code commit

---
+++ xetra/common/meta_process.py
@@ -0,0 +1,16 @@
+"""
+Methods for processing meta files
+"""
+
+class MetaProcess():
+    """
+    class for working with meta files
+    """
+    
+    @staticmethod
+    def update_meta_files():
+        pass
+    
+    @staticmethod
+    def return_date_list():
+        pass
