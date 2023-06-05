e37c9d64bb608b0282d5ce63703221e17d7362d5
Saurabh Joshi <saurabhjoshi7869@gmail.com>
2023-06-05 21:10:50 +0530

    added 'WrongFormatException' custom exception

--- xetra/common/custom_exceptions.py
+++ xetra/common/custom_exceptions.py
@@ -1,3 +1,9 @@
-"""
-Custom Exceptions
-"""
+""" Custom Exceptions"""
+
+class WrongFormatException(Exception):
+    """
+    WrongFormatException class
+    
+    Exception that can be raised when the format type
+    given as a parameter is not supported
+    """
